03752ce4736e3dce0aa3aff648838678faa1f241
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-11 08:59:25 -0300

    mainsetup finished

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { prisma } from "../src/prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function seed() {
   try {
--- src/domain/Projects/services/projectService.test.ts
+++ src/domain/Projects/services/projectService.test.ts
@@ -1,3 +1,13 @@
+import { PrismaClient, type Project } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const projectService = {
+  createProject: (args: Project): Promise<Project> => {
+    return prisma.project.create({ data: args });
+  },
+};
+
 describe("createProject", () => {
   test("should create a new project", async () => {
     expect(true).toBe(true);
