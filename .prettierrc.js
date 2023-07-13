a62c6106e89e316c8f126635d36e5772fa303297
Renato Takao Matura de Lima <renato.matura@gmail.com>
2023-07-13 00:08:48 -0300

    Removed unecessary comments

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,6 @@
 /** @type {import("@trivago/prettier-plugin-sort-imports").PrettierConfig} */
 
 const prettierConfig = {
-  // "prettier/prettier": [
-  //   "error",
-  //   {
-  //     "endOfLine": "auto"
-  //   }
-  // ],
   printWidth: 80,
   tabWidth: 2,
   semi: true,
@@ -14,17 +8,17 @@ const prettierConfig = {
   trailingComma: "es5",
   bracketSpacing: true,
   importOrder: [
-    '<THIRD_PARTY_MODULES>',
-    '^@/assets/(.*)$',
-    '^@/components/(.*)$',
-    '^@/constants/(.*)$',
-    '^@/styles/(.*)$',
-    '^@/types/(.*)$',
-    '^@/(.*)$',
-    '^[./]',
+    "<THIRD_PARTY_MODULES>",
+    "^@/assets/(.*)$",
+    "^@/components/(.*)$",
+    "^@/constants/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/types/(.*)$",
+    "^@/(.*)$",
+    "^[./]",
   ],
   importOrderCaseInsensitive: true,
   importOrderSeparation: true,
-}
+};
 
 module.exports = prettierConfig;
