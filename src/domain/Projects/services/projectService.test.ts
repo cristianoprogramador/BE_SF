8daf225e16a5e544ee01e86aea63d9c3db7a5250
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-13 22:43:58 -0300

    initial refactory with projects

--- src/domain/Projects/services/projectService.test.ts
+++ src/domain/Projects/services/projectService.test.ts
@@ -1,13 +1,3 @@
-import { PrismaClient, type Project } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export const projectService = {
-  createProject: (args: Project): Promise<Project> => {
-    return prisma.project.create({ data: args });
-  },
-};
-
 describe("createProject", () => {
   test("should create a new project", async () => {
     expect(true).toBe(true);
